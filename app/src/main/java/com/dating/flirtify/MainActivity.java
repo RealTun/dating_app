0238f6d34e6658de5abd4c7098f06fe83bbc6dde
DungSatoru <dungsatoru.dev@gmail.com>
2024-05-13 21:07:07 +0700

    Main Activity

--- app/src/main/java/com/dating/flirtify/MainActivity.java
+++ app/src/main/java/com/dating/flirtify/MainActivity.java
@@ -1,6 +1,13 @@
 package com.dating.flirtify;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        Shader textShader = new LinearGradient(
+//                0, 0, 0, 200,
+//                new int[]{Color.parseColor("#E32F70"), Color.parseColor("#E64B77"), Color.parseColor("#EE8585")},
+//                new float[]{0, 0.5f, 1},
+//                Shader.TileMode.CLAMP
+//        );
+//
+        TextView tvAppName = findViewById(R.id.textView);
+//        tvAppName.getPaint().setShader(textShader);
+
+        setColorGradient(tvAppName, getResources().getColor(R.color.gradient_top), getResources().getColor(R.color.gradient_center), getResources().getColor(R.color.gradient_bottom));
+
+
+
+    }
+
+    private void setColorGradient(TextView tv, int... color){
+        TextPaint textPaint = tv.getPaint();
+        float width = textPaint.measureText(tv.getText().toString());
+        float height = tv.getTextSize();
+
+        Shader textShader = new LinearGradient(
+                0, 0, 0, height,
+                color, null, Shader.TileMode.CLAMP
+        );
+
+        tv.getPaint().setShader(textShader);
+        tv.setTextColor(color[0]);
     }
 }
