cf5411e214ad36294d814542d6836068c05ff29a
Sweety <ledinhtu880@gmail.com>
2024-05-17 11:22:32 +0700

    styling preview user

---
+++ app/src/main/java/com/dating/flirtify/Fragments/HeaderFragment.java
@@ -0,0 +1,22 @@
+package com.dating.flirtify.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dating.flirtify.R;
+
+public class HeaderFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_header, container, false);
+    }
+}
---
+++ app/src/main/java/com/dating/flirtify/PreviewActivity.java
@@ -0,0 +1,47 @@
+package com.dating.flirtify;
+
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Bundle;
+import android.text.TextPaint;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PreviewActivity extends AppCompatActivity {
+
+    TextView tvAppName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_preview);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvAppName = findViewById(R.id.tvAppName);
+        setColorGradient(tvAppName, getResources().getColor(R.color.gradient_top), getResources().getColor(R.color.gradient_center), getResources().getColor(R.color.gradient_bottom));
+    }
+
+    private void setColorGradient(TextView tv, int... color) {
+        TextPaint textPaint = tv.getPaint();
+        float width = textPaint.measureText(tv.getText().toString());
+        float height = tv.getTextSize();
+
+        Shader textShader = new LinearGradient(
+                0, 0, 0, height,
+                color, null, Shader.TileMode.CLAMP
+        );
+
+        tv.getPaint().setShader(textShader);
+        tv.setTextColor(color[0]);
+    }
+}
